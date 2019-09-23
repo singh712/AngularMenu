909ef584e2a33c21a41375ff4875bfbbfd219c44
charanjit712 <singh285907@gmail.com>
2019-09-23 01:00:29 -0600

    assignment 2 task 3 completed

--- src/app/shared/leaders.ts
+++ src/app/shared/leaders.ts
@@ -39,6 +39,6 @@ export const LEADERS: Leader[] = [
       abbr: 'EC',
       featured: true,
       // tslint:disable-next-line:max-line-length
-      description: 'Award winning three-star Michelin chef with wide International experience having worked closely with whos-who in the culinary world, he specializes in creating mouthwatering Indo-Italian fusion experiences. He says, Put together the cuisines from the two craziest cultures, and you get a winning hit! Amma Mia!'
+      description: 'Award winning three-star Michelin chef with wide International experience having worked closely with whos-who in the culinary world, he specializes in creating mouthwatering Indo-Italian fusion experiences. He says, Put together the cuisines '
     }
   ];
