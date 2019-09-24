a60eb15b29c974d4d708e58077d0135ac9fa4e53
charanjit712 <singh285907@gmail.com>
2019-09-24 17:45:44 -0600

    server side delay rendering promise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatInputModule}   from '@angular/material/input';
 import {MatCheckboxModule}   from  '@angular/material/checkbox';
 import  {MatSelectModule} from '@angular/material/select';
 import {MatSlideToggleModule}  from '@angular/material/slide-toggle';
+import {MatProgressSpinnerModule}  from '@angular/material/progress-spinner';
 import {FormsModule }    from  '@angular/forms';
 import  {ReactiveFormsModule} from "@angular/forms";
 
@@ -71,6 +72,7 @@ import { LoginComponent } from './login/login.component';
     ReactiveFormsModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule,
 
   ],
   providers: [
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import {Dish} from '../shared/dish';
 import {DISHES} from '../shared/dishes';
+import { resolve } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +12,28 @@ export class DishService {
   constructor() { }
 
   getDishes(): Promise <Dish[]> {
-    return  Promise.resolve(DISHES);
+    return  new Promise( resolve => {
+// simulate server latency 2 sec 
+setTimeout(() => resolve(DISHES), 2000);
+
+    });
+       
   }
 
-  getDish(id: string) : Promise<Dish> {
+  getDish(id: string): Promise<Dish> {
 
-    return Promise.resolve(DISHES.filter((dish)  =>  (dish.id === id))[0]);
+    return  new Promise(resolve  => {
+      
+      setTimeout(() =>  resolve (DISHES.filter((dish)  =>  (dish.id === id))[0]),2000);
+    });
 
   }
   getFeaturedDish(): Promise<Dish> {
 
-    return Promise.resolve(DISHES.filter((dish)  =>  dish.featured)[0]);
+    return  new Promise (resolve => {
+      
+      setTimeout(() => resolve(DISHES.filter((dish)  =>  dish.featured)[0]),2000);
 
-  }
+  });
+}
 }
